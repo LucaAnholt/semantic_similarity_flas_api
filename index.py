534b1b57dc513792bad113dd83cb134d9ff7a093
Luca Anholt <la1317@ic.ac.uk>
2023-02-11 15:57:34 +0000

    push api

---
+++ index.py
@@ -0,0 +1,26 @@
+import spacy
+from flask import Flask, request
+import urllib.parse
+
+app = Flask(__name__)
+
+# Load the medium Spacy model
+nlp = spacy.load("en_core_web_md")
+
+@app.route("/GetScore", methods=["GET"])
+def get_score():
+    # Get the "user_answer" and "correct_answer" query parameters
+    user_answer = urllib.parse.quote(request.args.get("user_answer"))
+    correct_answer = urllib.parse.quote(request.args.get("correct_answer"))
+    user_answer = str(urllib.parse.unquote(user_answer).strip('"').lower())
+    correct_answer = str(urllib.parse.unquote(correct_answer).strip('"').lower())
+    # Compute the similarity score using the Spacy similarity method
+    doc1 = nlp(user_answer)
+    doc2 = nlp(correct_answer)
+    similarity_score = doc1.similarity(doc2)
+    
+    # Return the similarity score
+    return str(similarity_score)
+
+if __name__ == "__main__":
+    app.run(debug=True)
