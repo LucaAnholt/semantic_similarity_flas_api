ea54536b0d0bb635d29e747086cd14686dc352fa
Luca Anholt <luca.anholt@engitix.com>
2023-03-18 16:28:42 +0000

    separate schedule script into new docker image

--- schedule_url_answers.py
+++ schedule_url_answers.py
@@ -22,15 +22,14 @@ def next_url():
         # Get the URL and answer for the current row
         current_row += 1
 
-    print(rows[current_row])
-
     # Save the current row to a file
     with open('current_row.txt', 'w') as f:
         f.write(str(current_row))
 
 # Schedule the job to run at 9am every day
-schedule.every().day.at("09:00").do(next_url)
+#schedule.every().day.at("09:00").do(next_url)
+schedule.every(5).seconds.do(next_url)
 
 while True:
     schedule.run_pending()
-    time.sleep(3600)
+    time.sleep(5) #3600
